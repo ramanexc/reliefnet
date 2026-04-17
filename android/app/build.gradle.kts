624dafc815b86cbac79752b602096dcdec4cf7f0
Japneet2006 <japneetaries@gmail.com>
2026-04-17 23:40:58 +0530

    json

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,7 +46,7 @@ flutter {
 
 dependencies {
     // Import the Firebase BoM
-    implementation(platform("com.google.firebase:firebase-bom:34.11.0"))
+    implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
 
     // Add the dependencies for Firebase products you want to use
     // When using the BoM, don't specify versions in Firebase dependencies
